d859b51df373804d24913d4eba617589178a35d7
德华 刘 <gavinprior5@gmail.com>
2025-05-20 18:05:49 +0000

    create and protect dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { StudentProfileBentoGrid } from "@/components/profile-bento-grid/profile-bento";
+
+export default function page() {
+  return (
+    <section className="mt-32 mx-4">
+      <StudentProfileBentoGrid />
+    </section>
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/select-role"]);
+const isProtectedRoute = createRouteMatcher(["/select-role", "/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth(); // Await the auth promise
